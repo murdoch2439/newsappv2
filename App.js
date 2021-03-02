db971a222698617fbd8f9086af03695e973ab4d7
mr2k Ic <murdochkhallz@gmail.com>
2021-03-02 18:06:18 +0100

    Deleting view and text elements from the entry point of the App because they are useless at the curent moment

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeStack from './src/homeStack'
 import LandingPageScreen from './src/screens/landingPageScreen';
