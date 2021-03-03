7dff3a97ba5257f4c840d448f2f75e0bb6dad53a
mr2k Ic <murdochkhallz@gmail.com>
2021-03-03 14:32:28 +0100

    Creation of two fields to hold username and numberphone and the custom button

--- src/screens/connection/formScreen.js
+++ src/screens/connection/formScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View,StyleSheet, Text, Button} from 'react-native';
+import {View,StyleSheet, Text, Image, TextInput, TouchableOpacity} from 'react-native';
 
 const FormScreen = ({navigation}) =>{
   const goToConfirmationScreen = () =>{
@@ -7,8 +7,18 @@ const FormScreen = ({navigation}) =>{
   }
   return(
     <View style={styles.container}>
-      <Text>Form Screen</Text>
-      <Button title="Go to confirmation" onPress={goToConfirmationScreen} />
+      <View style={styles.imageContainer}>
+        <Image source={require('../../../assets/logo.jpg')} style={styles.image} />
+      </View>
+      <View style={styles.descriptionContainer}>
+        <Text>Le plus grand site d'information en RDC</Text>
+      </View>
+        <TextInput placeholder="*Nom d'utilisateur" style={styles.textInput} />
+        <TextInput placeholder="*Telephone" style={styles.textInput} />
+      <TouchableOpacity style={styles.button} onPress={goToConfirmationScreen}>
+        <Text style={styles.buttonText}>Registrate</Text>
+      </TouchableOpacity>
+      {/* <Button title="Go to confirmation"  /> */}
     </View>
   )
 }
@@ -16,6 +26,44 @@ const FormScreen = ({navigation}) =>{
 const styles = StyleSheet.create({
   container:{
     flex:1,
+    paddingTop:100,
+    backgroundColor:'white',
+  },
+  imageContainer:{
+    flexDirection:'row',
+    justifyContent:'center'
+  },
+  descriptionContainer:{
+    marginVertical:10,
+    justifyContent:'center',
+    flexDirection:'row',
+  },
+  image:{
+    height:50,
+    width:50,
+    borderRadius:10,
+  },
+  textInput:{
+    fontSize:18,
+    height:50,
+    marginBottom:10,
+    borderBottomWidth:1,
+    borderColor:'purple',
+    marginHorizontal:20,
+  },
+  button:{
+    marginLeft:20,
+    backgroundColor:'#72A52F',
+    height:60,
+    width:320,
+    borderRadius:5,
+    marginTop:190,
+  },
+  buttonText:{
+    color:'white',
+    textAlign:'center',
+    fontSize:25,
+    paddingTop:12,
   },
 })
 
