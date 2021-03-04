10a94705609e088782e3214c0830e4c3dd54d92d
mr2k Ic <murdochkhallz@gmail.com>
2021-03-04 11:32:15 +0100

    Adding a share button that allows user to share any post into other plateforms

--- src/homeStack.js
+++ src/homeStack.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/homeScreen';
 import DetailsScreen from './screens/detailsScreen'
 import HomeHeader from './components/homeHeader'
+import { AntDesign } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 
@@ -10,9 +12,17 @@ const HomeStack = () => {
   return (
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen}  options={{ header: ({navigation})=> <HomeHeader navigation={navigation} />}} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} options={{headerRight: ()=> <AntDesign name="sharealt" style={styles.share} onPress={()=> alert('Hello do you have something to share!')} />}} />
       </Stack.Navigator>
   );
 }
+
+const styles= StyleSheet.create({
+  share:{
+    fontSize:25,
+    color:'black',
+    marginRight:20,
+  },
+})
 
 export default HomeStack;
