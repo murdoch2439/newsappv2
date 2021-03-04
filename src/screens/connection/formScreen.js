896e023b0face2097fe5b2d11f5e07afed1e07fc
mr2k Ic <murdochkhallz@gmail.com>
2021-03-04 16:14:41 +0100

    handling user inputs using hooks

--- src/screens/connection/formScreen.js
+++ src/screens/connection/formScreen.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View,StyleSheet, Text, Image, TextInput, TouchableOpacity} from 'react-native';
 
 const FormScreen = ({navigation}) =>{
+  const [name, setName] = useState()
+  const [phoneNumber, setPhoneNumber] = useState()
+
+  const retrieveUsername = (newValue) =>{
+    setName(newValue)
+  }
+  const retrievePhoneNumber = (newValue) =>{
+    setPhoneNumber(newValue)
+  }
+
   const goToConfirmationScreen = () =>{
+    console.log(`Tu t'appelle ${name} et voici ton numero ${phoneNumber}`);
       navigation.navigate('Confirmation')
   }
   return(
@@ -11,14 +22,29 @@ const FormScreen = ({navigation}) =>{
         <Image source={require('../../../assets/logo.jpg')} style={styles.image} />
       </View>
       <View style={styles.descriptionContainer}>
-        <Text>Le plus grand site d'information en RDC</Text>
+        <Text style={styles.description}>Le plus grand site d'infos en RDC</Text>
       </View>
-        <TextInput placeholder="*Nom d'utilisateur" style={styles.textInput} />
-        <TextInput placeholder="*Telephone" style={styles.textInput} />
-      <TouchableOpacity style={styles.button} onPress={goToConfirmationScreen}>
-        <Text style={styles.buttonText}>Registrate</Text>
-      </TouchableOpacity>
-      {/* <Button title="Go to confirmation"  /> */}
+      <View style={styles.textInputContainer}>
+        <TextInput 
+          placeholder="*Nom d'utilisateur" 
+          style={styles.textInput} 
+          autoCapitalize="words" 
+          autoCompleteType="off"
+          onChangeText={retrieveUsername}
+        />
+        <TextInput
+          placeholder="*Telephone"
+          style={styles.textInput}
+          keyboardType="phone-pad"
+          maxLength={10}
+          onChangeText={retrievePhoneNumber}
+        />
+        {name || phoneNumber ? <Text>{name}, ton numero c'est {phoneNumber}</Text>: null}
+        <TouchableOpacity style={styles.button} onPress={goToConfirmationScreen}>
+          <Text style={styles.buttonText}>S'inscrire</Text>
+        </TouchableOpacity>
+      </View>
+      
     </View>
   )
 }
@@ -26,7 +52,7 @@ const FormScreen = ({navigation}) =>{
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    paddingTop:100,
+    paddingTop:30,
     backgroundColor:'white',
   },
   imageContainer:{
@@ -34,14 +60,21 @@ const styles = StyleSheet.create({
     justifyContent:'center'
   },
   descriptionContainer:{
-    marginVertical:10,
+    marginBottom:10,
     justifyContent:'center',
     flexDirection:'row',
   },
+  description:{
+    fontStyle:'italic',
+    fontSize:16,
+  },
   image:{
-    height:50,
-    width:50,
+    height:70,
+    width:70,
     borderRadius:10,
+  },
+  textInputContainer:{
+    marginTop:30
   },
   textInput:{
     fontSize:18,
@@ -53,17 +86,18 @@ const styles = StyleSheet.create({
   },
   button:{
     marginLeft:20,
-    backgroundColor:'#72A52F',
+    backgroundColor:'#73A532', 
     height:60,
     width:320,
     borderRadius:5,
-    marginTop:190,
+    marginTop:170,
   },
   buttonText:{
     color:'white',
     textAlign:'center',
     fontSize:25,
     paddingTop:12,
+    fontWeight:'bold'
   },
 })
 
