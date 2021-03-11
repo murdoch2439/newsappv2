3da7bd48ea7332f1d7d94ff648311b4607e532c2
mr2k Ic <murdochkhallz@gmail.com>
2021-03-11 15:55:08 +0100

    Adding a image to show when the info contains no image and setting the statement of it and also when the info has no label to print

--- src/components/infoCard.js
+++ src/components/infoCard.js
@@ -3,7 +3,7 @@ import {View, StyleSheet, Text, Image, TouchableOpacity} from 'react-native';
 import { Fontisto } from '@expo/vector-icons';
 import { Fontisto as one } from '@expo/vector-icons';
 
-const InfoCard = () =>{
+const InfoCard = ({item, navigation}) =>{
   const [addBookmark, setAddbookmark] = useState(false)
   
   const saveToFavorite = () =>{
@@ -14,25 +14,27 @@ const InfoCard = () =>{
   }
   return(
     <View style={styles.container}>
-      <View>
-      <View>
-       <Image source={require('../../assets/banniere.jpg')} style={styles.image} />
-        <View style={styles.labelContainer}>
-          <Text style={styles.label}>Sociale</Text>
-          {/* <TouchableOpacity onPress={saveToFavorite}> */}
+      <TouchableOpacity onPress={()=> navigation.navigate('Details')}>
           
-          {addBookmark ? <Fontisto name="bookmark-alt" style={styles.bookMarkcolored} onPress={deleteToFavorite} /> : <Fontisto onPress={saveToFavorite}   name="bookmark" style={styles.bookMark} />   }
-          {/* </TouchableOpacity> */}
-          <Text style={styles.date}>mar 05/03/2021 - 15:28</Text>
+          <View style={styles.imageContainer}>
+            {!item.urlToImage ? <Image source={require('../../assets/noImage.png')} style={{width:200, height:200, backgroundColor:'gray', marginHorizontal: 70}} />:<Image source={{uri:item.urlToImage}} style={styles.image} />}
+          </View>
+
+          <View style={styles.bookMarkContainer}>
+            {addBookmark ? <Fontisto name="bookmark-alt" style={styles.bookMarkcolored} onPress={deleteToFavorite} /> : <Fontisto onPress={saveToFavorite}   name="bookmark" style={styles.bookMark} />   }
+          </View>
+
+          <View style={styles.labelAndDateContainer}>
+             {item.author ? <Text style={styles.label}>{item.author}</Text> : <Text style={styles.label1}></Text>}          
+            <Text style={styles.date}>{item.publishedAt}</Text>
+          </View>
+          {/* <Text style={styles.title} numberOfLines={4}>RDC: Here goes the info title that describe well the article, this should fit on 4 lines only I wish it will be good enough for people to enjoy what will be shown here I've got nothing left to say but I need to say something to fill the space and check out what the output will look like, that why I'm sorry to joke on you  </Text> */}
           
-        </View>
-      </View>
-        <Text style={styles.title} numberOfLines={4}>RDC: Here goes the info title that describe well the article, this should fit on 3 lines only bka jdhdb herhru hffjk hjfhfjjr jfjjf jdkddj jdjdkjdn jdjdkjd kjghfg j dkj djdkkd jdkkd dkkd jkkdkd djjd</Text>
-      </View>
+          <Text style={styles.title} numberOfLines={4}>{item.description}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container:{
@@ -41,13 +43,48 @@ const styles = StyleSheet.create({
     margin:15,
     borderRadius:5,
   },
-  labelContainer:{
+  imageContainer:{
+    backgroundColor:'gray',
+    width:'100%',
+    borderRadius:10,
+  },
+  image:{
+    width:'100%',
+    height:180,
+    borderTopRightRadius:5,
+    borderTopLeftRadius:5,
+  },
+  bookMarkContainer:{
+    // backgroundColor:'pink',
+    position:'absolute',
+    right:10,
+    top:10,
+    
+  },
+  bookMark:{
+    fontSize:35,
+    color:'white',
+    
+    // bottom:180,
+    // left:170,
+    //An other layout can be to set both of label and bookmark at picture top
+    /* -put labelContainer:{
+      position : 'absolute'
+    }  
+       -bookMark:{
+      left: 230
+       }
+    */
+  },
+  bookMarkcolored:{
+    fontSize:35,
+    color:'white',
+  },
+  labelAndDateContainer:{
     flexDirection:'row',
     justifyContent:'space-between',
-    paddingHorizontal:10,
     paddingTop:10,
-    // position:'absolute',
-    
+    paddingHorizontal:5,
   },
   label:{
     color:'white',
@@ -56,50 +93,24 @@ const styles = StyleSheet.create({
     borderRadius:2,
     fontWeight:'700',
     marginBottom:15,
-    // bottom:180,
-    right:5
-    
-    // bottom:10,
-    // right:5
   },
-  bookMark:{
-    fontSize:35,
+  label1:{
     color:'white',
-    bottom:180,
-    left:180,
-    //An other layout can be to set both of label and bookmark at picture top
-    /* -put labelContainer:{
-      position : 'absolute'
-    }  
-       -bookMark:{
-      left: 230
-       }
-    
-    */
-  },
-  bookMarkcolored:{
-    fontSize:35,
-    color:'#72A52F',
-    bottom:190,
-    left:180,
+    paddingHorizontal:5,
+    borderRadius:2,
+    fontWeight:'700',
+    marginBottom:15,
   },
   date:{
     fontStyle:'italic',
     fontSize:12,
-  },
-  image:{
-    width:'100%',
-    height:180,
-    borderTopRightRadius:5,
-    borderTopLeftRadius:5,
+    alignItems:'center'
   },
   title:{
-    fontWeight:'600',
+    fontWeight:'bold',
     marginHorizontal:5,
-    marginTop:-10,
     fontSize:15,
     textAlign:'justify',
-    //overflow:'hidden'
   }
 })
 
