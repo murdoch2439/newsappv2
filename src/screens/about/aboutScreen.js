ac55092f240e04b626a0e05c87f6cc834bd6eb04
mr2k Ic <murdochkhallz@gmail.com>
2021-03-12 19:27:39 +0100

    adding some content on about screen

--- src/screens/about/aboutScreen.js
+++ src/screens/about/aboutScreen.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
 
 
 const AboutScreen = () =>{
   return(
     <View style={styles.container}>
-      <Text>About Screen</Text>
+      <View>
+
+        <Text>Qui nous sommes</Text>
+        <Text>7SUR7.CD est le plus grand site d'infos en RDC.</Text>
+        <Text>Avec plus de 100.000 visites par jour</Text>
+        <Text>Plus de 312.200 fans sur facebook</Text>
+        <Text>Plus de 199.511 followers sur Twitter</Text>
+
+        <Text>Nous contacter</Text>
+        <Text>Telephone: +243 81 6661215</Text>
+        <Text>Mail: contact@7sur7.cd</Text>
+        <TouchableOpacity>Equipe redactionnelle</TouchableOpacity>
+      </View>
+
+      <View>
+
+        <TouchableOpacity>
+          <Text>Rejoindre le groupe Telegram</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   )
 }
