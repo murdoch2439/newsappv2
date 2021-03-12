ab85eee181533b039286ebb070e39e94f32f36ef
mr2k Ic <murdochkhallz@gmail.com>
2021-03-12 17:36:21 +0100

    adding activity indicator with settimeout

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,15 +1,70 @@
-import React from 'react'
-import { View, StyleSheet, Text, Button, ScrollView } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import { View, StyleSheet, Button, FlatList, ActivityIndicator, TouchableNativeFeedback } from 'react-native'
 import InfoCard from '../components/infoCard'
+import newApi from '../../api/actuApi'
+
 
 
 const HomeScreen = ({navigation}) => {
-    return(
-        <View style = { styles.container }>
-            {/* <Text> Hello It is the Home Screen</Text> */}
+    const [news, setNews] = useState([])
+    const [loading, setLoading] = useState(true)
+    
+    useEffect(()=>{
+        setTimeout(()=>{
+
+            getNewsFromAPI()
+        }, 1000)
+        
+
+    }, [])
+    const getNewsFromAPI = () =>{
+        newApi.get('everything?q=Apple&from=2021-03-11&sortBy=popularity&apiKey=3e37e8dc20f14aeb8e557b2c61454855')
+        .then( async (response) => {
+            setNews(response.data)
+            setLoading(false);
+        })
+        .catch((error) => {
+            console.log(error);
             
-            <InfoCard />
-            <InfoCard />
+        })
+    }
+
+    if(!news){
+        return null
+    }
+
+    const renderItem = ({item}) =>{
+                    return(
+                        
+                            <InfoCard item={item}  />
+                        
+                        
+                    )
+                }
+
+
+    return(
+        
+
+        <View style = { styles.container }>           
+            
+            <FlatList 
+                data={news.articles}
+                keyExtractor={(article) => article.description}
+                renderItem ={renderItem}
+                ListEmptyComponent={() => {
+                    if (loading) {
+                        return (
+                        <View style={styles.loading}>
+                            <ActivityIndicator size="large"  color="#72A52F" />
+                        </View>
+                        );
+                    }
+
+                    return null;
+                }}
+            />
+            
             <Button title="Go to Details Screen" onPress={()=> navigation.navigate('Details')}/> 
         </View>
     )
@@ -18,8 +73,13 @@ const HomeScreen = ({navigation}) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#f0efeb'
+        backgroundColor:'#CCCCCC'
 
+    },
+    loading: {
+        marginVertical: 250,
+        justifyContent:'center',
+        alignItems:'center',
     },
 })
 
