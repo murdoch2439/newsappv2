a9167c0a935d06f3f15f9a3f20ef7b69c2823f6a
mr2k Ic <murdochkhallz@gmail.com>
2021-03-12 17:35:20 +0100

    adding share component on detailsScreen

--- src/homeStack.js
+++ src/homeStack.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import DetailsScreen from './screens/detailsScreen'
 import HomeHeader from './components/homeHeader'
-import { AntDesign } from '@expo/vector-icons';
 import TabNavigationStack from './screens/tabNavigation/tabNavigationStack';
+import ShareButton from './components/shareButton';
 
 const Stack = createStackNavigator();
 
@@ -12,17 +11,11 @@ const HomeStack = () => {
   return (
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={TabNavigationStack}  options={{ header: ({navigation})=> <HomeHeader navigation={navigation} />}} />
-        <Stack.Screen name="Details" component={DetailsScreen} options={{headerRight: ()=> <AntDesign name="sharealt" style={styles.share} onPress={()=> alert('Hello do you have something to share!')} />}} />
+        <Stack.Screen name="Details" component={DetailsScreen} options={{headerRight: ()=> <ShareButton />}} />
       </Stack.Navigator>
   );
 }
 
-const styles= StyleSheet.create({
-  share:{
-    fontSize:25,
-    color:'black',
-    marginRight:20,
-  },
-})
+
 
 export default HomeStack;
