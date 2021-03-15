1adcc50840c743946932ccde90ca675c5e48377c
mr2k Ic <murdochkhallz@gmail.com>
2021-03-15 19:51:05 +0100

    Putting change on the screen and adding a button for joining telegram

--- src/screens/about/aboutScreen.js
+++ src/screens/about/aboutScreen.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 
 const AboutScreen = () =>{
   return(
     <View style={styles.container}>
-      <View>
+      <View style={styles.textContainer}>
 
-        <Text>Qui nous sommes</Text>
-        <Text>7SUR7.CD est le plus grand site d'infos en RDC.</Text>
-        <Text>Avec plus de 100.000 visites par jour</Text>
-        <Text>Plus de 312.200 fans sur facebook</Text>
-        <Text>Plus de 199.511 followers sur Twitter</Text>
+        <Text style={styles.title}>Qui nous sommes</Text>
+        <Text style={styles.text}>Nous sommes le plus grand site d'infos en RDC.</Text>
+        <Text style={styles.text}>Avec plus de 100.000 visites par jour</Text>
+        <Text style={styles.text}>Plus de 312.200 fans sur facebook</Text>
+        <Text style={styles.text}>Plus de 199.511 followers sur Twitter</Text>
 
-        <Text>Nous contacter</Text>
-        <Text>Telephone: +243 81 6661215</Text>
-        <Text>Mail: contact@7sur7.cd</Text>
-        <TouchableOpacity>Equipe redactionnelle</TouchableOpacity>
+        <Text style={styles.title}>Nous contacter</Text>
+        <Text style={styles.text}>Telephone: +243 81 6661215</Text>
+        <Text style={styles.text}>Mail: contact@7sur7.cd</Text>
+        <View>
+
+          <TouchableOpacity>
+              <Text style={styles.text}>Equipe redactionnelle</Text>
+          </TouchableOpacity>
+        <View>
+
+          <TouchableOpacity>
+            <Text style={styles.text}>Rejoindre le groupe Telegram</Text>
+          </TouchableOpacity>
+          <FontAwesome5 name="telegram-plane" size={24} color="black" />
+        </View>
+        </View>
       </View>
-
-      <View>
-
-        <TouchableOpacity>
-          <Text>Rejoindre le groupe Telegram</Text>
-        </TouchableOpacity>
-      </View>
-
     </View>
   )
 }
@@ -34,6 +39,21 @@ const AboutScreen = () =>{
 const styles = StyleSheet.create({
   container:{
     flex:1,
+    backgroundColor:'#72A52F',
+
+  },
+  textContainer:{
+    justifyContent:"center",
+    alignItems:'center',
+  },
+  title:{
+    fontWeight:'800',
+    fontSize:20,
+    color:'white',
+  },
+  text:{
+    color:'white',
+    fontSize:16
   },
 })
 
