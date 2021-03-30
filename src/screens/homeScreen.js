11c85bd852ed206a66da159d3d5ab3f49c863ab1
mr2k Ic <murdochkhallz@gmail.com>
2021-03-30 17:50:25 +0100

    Adding an endpoint to consume ressources from articles/all

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { View, StyleSheet, Button, FlatList, ActivityIndicator, TouchableNativeFeedback } from 'react-native'
 import InfoCard from '../components/infoCard'
 import newApi from '../../api/actuApi'
+import axios from 'axios'
 
 
 
@@ -18,22 +19,24 @@ const HomeScreen = ({navigation}) => {
 
     }, [])
     const getNewsFromAPI = () =>{
-        newApi.get('everything?q=Apple&from=2021-03-11&sortBy=popularity&apiKey=3e37e8dc20f14aeb8e557b2c61454855')
+        axios.get('https://7sur7.cd/api/v1/articles/all')
         .then( async (response) => {
+            // console.log(response.data);
             setNews(response.data)
             setLoading(false);
+            
         })
         .catch((error) => {
             console.log(error);
-            
+    
         })
     }
 
-    if(!news){
-        return null
-    }
+    // if(!news){
+    //     return null
+    // }
 
-    const renderItem = item => <InfoCard item={item}  />;
+    const renderItem = ({item}) => <InfoCard item={item}  />;
                     
 
     return(
@@ -41,8 +44,8 @@ const HomeScreen = ({navigation}) => {
         <View style = { styles.container }>           
             
             <FlatList 
-                data={news.articles}
-                keyExtractor={(article) => article.description}
+                data={news}
+                keyExtractor={(article) => article.body}
                 renderItem ={renderItem}
                 ListEmptyComponent={() => {
                     if (loading) {
