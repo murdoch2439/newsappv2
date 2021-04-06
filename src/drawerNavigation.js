0d81a6c47dbef240d9c5489c918d212f2904625d
mr2k Ic <murdochkhallz@gmail.com>
2021-04-06 18:07:10 +0100

    Creating a custom drawer component that shows both of title and icon

--- src/drawerNavigation.js
+++ src/drawerNavigation.js
@@ -5,13 +5,25 @@ import NotificationsScreen from './screens/notificationScreen'
 import RegistrationStack from './screens/connection/registrationStack'
 import SavedInformationsScreen from './screens/SavedInformationsScreen'
 import AboutStack from './screens/about/aboutStack'
+import SideMenu from './navigations/sideMenu';
 // import HeaderWithBackButton from './components/headerwithbackbutton'
 
+
+
+
+// const getDrawerContent = ({navigation}) =>{
+//         return(
+//                 <SideMenu navigation={navigation} />
+//                 )
+//         }
+        
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigation = () =>{
         return(
-        <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Navigator initialRouteName="Home" drawerContent={({navigation}) =>{
+                 return (<SideMenu navigation={navigation} />)
+        }}>
                 <Drawer.Screen  name="Registration" component={RegistrationStack} />
                 <Drawer.Screen name="Home" component={HomeStack} />
                 <Drawer.Screen name="Notifications" component={NotificationsScreen} />
