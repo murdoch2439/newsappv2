afb1923ff2c0a5284c973168e79b2479621cfd03
mr2k Ic <murdochkhallz@gmail.com>
2021-04-06 18:19:18 +0100

    few style changes

--- src/screens/connection/registrationStack.js
+++ src/screens/connection/registrationStack.js
@@ -9,7 +9,7 @@ const RegistrationStack = () => {
   /*in case you want to hide the header on any screen use  headerShown and set to false */
   return (
       <Stack.Navigator initialRouteName="Registration">
-        <Stack.Screen name="Registration" component={FormScreen} options={{headerTitleAlign:'center'}} />
+        <Stack.Screen name="Registration" component={FormScreen} options={{headerShown:false}} />
         <Stack.Screen name="Confirmation" component={ConfirmationScreen}  options={{headerTitleAlign:'center'}}
          />
          
