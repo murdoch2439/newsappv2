0ced50d850138c85c4645825c098be4a4ff7ea30
mr2k Ic <murdochkhallz@gmail.com>
2021-04-08 20:03:10 +0100

    Nothing about these last three they've no worth

---
+++ src/api/ngrokServer.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+
+export default axios.create({
+    baseURL: 'http://defc6577ac15.ngrok.io'
+})
---
+++ src/context/infos.js
@@ -0,0 +1,8 @@
+// const [form, setForm] = useState({})
+
+//   const onchange = ({name, value}) =>{
+//     setForm({...form,[name]:value})
+
+//   }
+
+  // onChangeText={(value) =>{onchange({name:'userName', value:value})}}
