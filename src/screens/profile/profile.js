0f2d12f2a2c84055cd4a390133bef204a675f7a2
mr2k Ic <murdochkhallz@gmail.com>
2021-04-08 20:28:26 +0100

    This will contain the user profile screen with all of his informations

---
+++ src/screens/profile/profile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {View, StyleSheet, Text} from 'react-native'
+
+const UserProfileScreen = () =>{
+  return(
+    <View style={styles.container}>
+      <Text>UserProfileScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+})
+
+export default UserProfileScreen
