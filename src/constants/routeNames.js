5cb55085863fb334c6b54ac457f3ed2df9cc1b41
mr2k Ic <murdochkhallz@gmail.com>
2021-04-09 20:44:05 +0100

    Centralizing all my routes in a constant to retrieve them easily

---
+++ src/constants/routeNames.js
@@ -0,0 +1,23 @@
+export const USER_PROFILE = 'Profile';
+export const REGISTRATION = 'Registration';
+export const HOME_NAVIGATION = 'Home navigation';
+export const NOTIFICATIONS = 'Notifications';
+export const SAVED_INFOS = 'A lire plus tard';
+export const ABOUT = 'About';
+export const INFOS_DETAILS = 'Details';
+export const TOP_TAB_NAVIGATION = 'Top tab navigation';
+export const POLITIQUE = 'Politique'
+export const ECONOMIE = 'Economie';
+export const EDUCATION = 'Education';
+export const SANTE = 'Sante';
+export const SPORT = 'Sport';
+export const CULTURE = 'Culture';
+export const SECURITE = 'Securite';
+export const SOCIETE = 'Societe';
+export const A_LA_UNE = 'A la une';
+export const A_PROPOS_DE_NOUS = 'A propos de nous';
+export const LA_TEAM = "L'equipe";
+export const SIGN_UP = 'Inscription';
+export const CONFIRMATION = 'Confirmation';
+
+
