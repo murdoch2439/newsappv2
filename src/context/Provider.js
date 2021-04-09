b1ec3af08b9307d4becad51682bed35b3709ce76
mr2k Ic <murdochkhallz@gmail.com>
2021-04-09 20:48:19 +0100

    Using Context to manage the global state of the application, this will help me making the all application state in any component

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,1 +1,20 @@
-export default () => null;
+import React, {createContext, useReducer} from 'react'
+import newsInitialState from './initialStates/newsInitialState'
+import registrationInitialState from './initialStates/signUpInitialState'
+import newsReducer from './reducers/newsReducer'
+import registrationReducer from './reducers/registrationReducer'
+
+export const GlobalContext = createContext({})
+
+const GlobalProvider = ({children}) =>{
+  const [registrationState, registrationDispatch]= useReducer(registrationReducer, registrationInitialState)
+  const [newsState, newsDispatch]= useReducer(newsReducer, newsInitialState)
+
+  return(
+    <GlobalContext.Provider value={{registrationState, newsState, registrationDispatch, newsDispatch}}>
+      {children}
+    </GlobalContext.Provider>
+  )
+}
+
+export default GlobalProvider
