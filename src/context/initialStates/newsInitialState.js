9d8622c99230baea7ec6339862a1d126d1edbfb1
mr2k Ic <murdochkhallz@gmail.com>
2021-04-09 20:52:29 +0100

    Franckly I am not yet sure about the usage of these but I'm thinking about fetching news and putting it in the global state too, anyway I'll figure out what to do with

---
+++ src/context/initialStates/newsInitialState.js
@@ -0,0 +1,18 @@
+export default {
+  getNews:{
+    data:{},
+    errors:null,
+    loading:false
+  },
+  saveNewsToFavorites:{
+    data:{},
+    errors:null,
+    loading:false
+  },
+  deleteNewsFromFavorites:{
+    data:{},
+    errors:null,
+    loading:false
+  }
+
+}
---
+++ src/context/reducers/newsReducer.js
@@ -0,0 +1,14 @@
+const newsReducer = (state, {type, payload}) =>{
+
+  switch (type){
+    case "GET_NEWS":
+      return state;
+    
+    default :
+    return state;
+
+  }
+
+};
+
+export default newsReducer;
