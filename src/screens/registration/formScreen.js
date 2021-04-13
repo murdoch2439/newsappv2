20e6ad72302a56338c44e872f953ede86018877a
mr2k Ic <murdochkhallz@gmail.com>
2021-04-13 20:29:37 +0100

    I'm trying to follow a tuto for implementation but very soon I will refactor this to my personnal need using my own DB to store users

--- src/screens/registration/formScreen.js
+++ src/screens/registration/formScreen.js
@@ -1,27 +1,28 @@
-import React, {useState} from 'react';
-import {View,StyleSheet, Text, Image, TouchableWithoutFeedback, Keyboard, ScrollView} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View,StyleSheet, Text, Image, TouchableWithoutFeedback, Keyboard, ScrollView, } from 'react-native';
 import ButtonComponent from '../../components/common/Button';
 import {AntDesign} from '@expo/vector-icons'
-import axios from 'axios';
+// import axios from 'axios';
 import InputComponent from '../../components/common/textInput/Input';
 import { Feather } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import axiosInstance from '../../api/key';
 
 
 const FormScreen = ({navigation}) =>{
-  const [name, setName] = useState('')
+  const [userName, setUserName] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
   
 
-  const retrieveUsername = (newValue) =>{
-    setName(newValue)
+  const retrieveUserName = (newValue) =>{
+    setUserName(newValue)
   }
   const retrievePhoneNumber = (newValue) =>{
     setPhoneNumber(newValue)
   }
 
-  const goToConfirmationScreen = () =>{
-    console.log({"name":name, "numero":phoneNumber});
+  const onSubmit = () =>{
+    console.log({"name":userName, "number":phoneNumber});
     // axios.post('/somewhere', {name, phoneNumber})
     //   .then((res)=>{})
     //   .catch((error) =>{
@@ -33,61 +34,97 @@ const FormScreen = ({navigation}) =>{
   const dismissKeyboard = () =>{
     Keyboard.dismiss()
   }
+
+  //This was not working as expected
+
+// const [form, setForm] = useState({})
+//   const [errors, setErrors] = useState({})
+
+//   const onChange = ({name, value}) =>{
+//     setForm({...form, [name]:value})
+//   }
+
+//   const onSubmit = () =>{
+
+//   console.log("form: >>", form);
+
+//   }
+
+
+  useEffect(()=>{
+    axiosInstance.post('/auth/registration').catch((error)=>{
+      console.log(error);
+    })
+  }, [])
+
+
   return(
     
     <TouchableWithoutFeedback  onPress={dismissKeyboard}>
       <View style={styles.container} >
-      <ScrollView showsVerticalScrollIndicator={false}>
-
-        <View style={styles.imageContainer}>
-          <Image source={require('../../../assets/logo.jpg')} style={styles.image} />
-        </View>
-        <View style={styles.descriptionContainer}>
-          <Text style={styles.description}>
-            Le plus grand site d'infos en
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={styles.imageContainer}>
+            <Image source={require('../../../assets/logo.jpg')} style={styles.image} />
+          </View>
+          <View style={styles.descriptionContainer}>
+            <Text style={styles.description}>
+                Le plus grand site d'infos en
             <Text style={styles.rdc}> RDC</Text>
-          </Text>
-        </View>
-        <View style={styles.textInputContainer}>
-          <InputComponent 
-            placeholder="*Nom d'utilisateur" 
-            style={styles.textInput} 
-            autoCapitalize="words" 
-            autoCompleteType="off"
-            onChangeText={retrieveUsername}
-            value={name}
-            icon={<Feather name="user" size={24} color="black" />}
-          />
-          <InputComponent
-            placeholder="*Telephone"
-            style={styles.textInput}
-            keyboardType="phone-pad"
-            maxLength={10}
-            onChangeText={retrievePhoneNumber}
-            value={phoneNumber}
-            icon={<MaterialCommunityIcons name="cellphone-key" size={24} color="black" />}
-          />
+            </Text>
+          </View>
+          <View style={styles.textInputContainer}>
+            {/* <Text style={{textAlign:'center', paddingTop:20, fontSize:20}}>Pour ne plus manquer l'actualité</Text> */}
+            <InputComponent 
+              placeholder="*Nom d'utilisateur" 
+              style={styles.textInput} 
+              autoCapitalize="words" 
+              autoCompleteType="off"
+              onChangeText={retrieveUserName}
+              value={userName}
+              // onChangeText={(value) =>{
+              //   onChange({name:"userName", value})
+              // }}
+              // value={form}
+              
+              // error={errors.firstName}
+              icon={<Feather name="user" size={24} color="black" />}
+            />
+            <InputComponent
+              placeholder="*Telephone"
+              style={styles.textInput}
+              keyboardType="phone-pad"
+              maxLength={10}
+              onChangeText={retrievePhoneNumber}
+              value={phoneNumber}
+              // onChangeText={(value)=>{
+              //   onChange({name:"phoneNumber", value})
+              // }}
+              // value={form}
+              icon={<MaterialCommunityIcons name="cellphone-key" size={24} color="black" />}
+            />
           
-          {
-            phoneNumber.length < 10 ?
-            <View style={styles.suggestionContainer}>
-              <Text style={styles.suggestionTextGray}>Le numero doit avoir 10 characteres </Text>
-              <AntDesign name="checkcircle" size={16} color="gray" />
-            </View> :
-            <View style={styles.suggestionContainer}>
-              <Text style={styles.suggestionText}>Le numero doit avoir 10 characteres </Text>
-              <AntDesign name="checkcircle" size={16} color="green" />
+            {
+              phoneNumber.length < 10 ?
+              <View style={styles.suggestionContainer}>
+                <Text style={styles.suggestionTextGray}>Le numero doit avoir 10 characteres </Text>
+                <AntDesign name="checkcircle" size={16} color="gray" />
+              </View> :
+              <View style={styles.suggestionContainer}>
+                <Text style={styles.suggestionTextGreen}>Le numero doit avoir 10 characteres </Text>
+                <AntDesign name="checkcircle" size={16} color="green" />
+              </View>
+            } 
+            {userName || phoneNumber ? <Text style={{marginLeft:20, fontSize:12, paddingTop:10}}>{userName}, ton numero c'est {phoneNumber}</Text>: null}
+            <View style={styles.separator} />
+              <ButtonComponent title="S'inscrire" loading={false} disabled={false} onPress={onSubmit} primary />
+              
+              {/* <ButtonComponent title="Click me" loading={true} disabled={true} onPress={goToConfirmationScreen} primary /> */}
+            
             </View>
-          }
-          {name || phoneNumber ? <Text style={{marginLeft:20, fontSize:12, paddingTop:10}}>{name}, ton numero c'est {phoneNumber}</Text>: null}
-          <View style={styles.separator} />
-          <ButtonComponent title="S'inscrire" onPress={goToConfirmationScreen} />
-          
-       </View>
         
-     </ScrollView>
-     </View>
-   </TouchableWithoutFeedback>
+        </ScrollView>
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
@@ -98,10 +135,14 @@ const styles = StyleSheet.create({
   },
   imageContainer:{
     flexDirection:'row',
-    justifyContent:'center'
+    justifyContent:'center',
+  },
+  image:{
+    height:70,
+    width:70,
+    borderRadius:10,
   },
   descriptionContainer:{
-    marginBottom:10,
     justifyContent:'center',
     flexDirection:'row',
   },
@@ -116,10 +157,8 @@ const styles = StyleSheet.create({
     fontWeight:'bold'
     
   },
-  image:{
-    height:70,
-    width:70,
-    borderRadius:10,
+  textInputContainer:{
+    // backgroundColor:'red'
   },
   suggestionContainer:{
     flexDirection:'row',
@@ -127,16 +166,18 @@ const styles = StyleSheet.create({
     marginTop:20,
 
   },
-  suggestionText:{
+  suggestionTextGreen:{
     color:'green',
     fontSize:12,
-    fontStyle:'italic'
+    fontStyle:'italic',
+    paddingRight:5,
 
   },
   suggestionTextGray:{
     color:'gray',
     fontSize:12,
-    fontStyle:'italic'
+    fontStyle:'italic',
+    paddingRight:5,
 
   },
   separator:{
