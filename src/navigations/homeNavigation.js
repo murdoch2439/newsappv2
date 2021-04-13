c17c60fde5f124e9c4c08efa6a78cb84dd41e3ba
mr2k Ic <murdochkhallz@gmail.com>
2021-04-13 20:23:43 +0100

    Changing the routes names, after to have set them together in a file of constants

--- src/navigations/homeNavigation.js
+++ src/navigations/homeNavigation.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import DetailsScreen from '../screens/detailsScreen'
 import HomeHeader from '../components/homeHeader'
-import TabNavigationStack from '../screens/tabNavigation/topTabNavigation';
+import TabNavigationStack from './TopTabNavigation';
 import ShareButton from '../components/shareButton';
+import { INFOS_DETAILS, TOP_TAB_NAVIGATION } from '../constants/routeNames';
 
 const HomeStack = createStackNavigator();
 
 const HomeNavigation = () => {
   return (
-      <HomeStack.Navigator initialRouteName="Home">
-        <HomeStack.Screen name="Home" component={TabNavigationStack}  options={{ header: ({navigation})=> <HomeHeader navigation={navigation} />}} />
-        <HomeStack.Screen name="Details" component={DetailsScreen} options={{headerRight: ()=> <ShareButton />}} />
+      <HomeStack.Navigator initialRouteName={TOP_TAB_NAVIGATION}>
+        <HomeStack.Screen name={TOP_TAB_NAVIGATION} component={TabNavigationStack}  options={{ header: ({navigation})=> <HomeHeader navigation={navigation} />}} />
+        <HomeStack.Screen name={INFOS_DETAILS} component={DetailsScreen} options={{headerRight: ()=> <ShareButton />}} />
       </HomeStack.Navigator>
   );
 }
