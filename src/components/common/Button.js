c02927b33a9a24e202c3ebf07c512f13069b3aeb
mr2k Ic <murdochkhallz@gmail.com>
2021-04-13 20:18:19 +0100

    New style of button with additional property that help to have suit the use case

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, ActivityIndicator} from 'react-native';
+import colors from '../theme/colors';
 
-const ButtonComponent = ({title, onPress}) =>{
+const ButtonComponent = ({title, loading, primary, secondary, danger, disabled, onPress}) =>{
+  const getBackgroundColor = () =>{
+    if(disabled){
+      return colors.grey
+    }
+    if(primary){
+      return colors.primary
+    }
+    if(secondary){
+      return colors.secondary
+    }
+    if(danger){
+      return colors.danger
+    }
+
+  }
   return(
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={onPress} >
-          <Text style={styles.buttonText}>{title}</Text>
-        </TouchableOpacity>
+      <TouchableOpacity
+        disabled={disabled}
+        loading={loading}
+        style={[styles.button,{backgroundColor: getBackgroundColor()}]} 
+        onPress={onPress} >
+          <View style={styles.loaderSection}>
+            {loading && <ActivityIndicator  color={colors.primary} />}
+            {title && 
+              <Text style={[styles.buttonText, {color:disabled?"black":colors.white, paddingLeft: loading ? 5: 0}]}>{title}</Text>}
+          </View>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -15,21 +39,23 @@ const styles = StyleSheet.create({
     height:100,
     marginHorizontal:10,
     justifyContent:'center',
-    alignItems:'center'
+    alignItems:'center',
   },
   button:{
     backgroundColor:'#73A532', 
     height:60,
     width:'100%',
     borderRadius:3,
-    justifyContent:'center'
-
+    alignItems:'center',
+    justifyContent:'space-evenly',
+  },
+  loaderSection:{
+    flexDirection:'row'
   },
   buttonText:{
-    color:'white',
     textAlign:'center',
-    fontSize:29,
-    fontWeight:'bold',
+    fontSize:25,
+    fontWeight:'700',
   },
 })
 
