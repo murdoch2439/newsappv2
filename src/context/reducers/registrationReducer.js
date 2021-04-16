7a71e7bcb5dcec7ace9b8a8f8e9a51424e1797d1
mr2k Ic <murdochkhallz@gmail.com>
2021-04-16 19:01:29 +0100

    Registration reducer to hold different case that can happen when a user tries to get registred

--- src/context/reducers/registrationReducer.js
+++ src/context/reducers/registrationReducer.js
@@ -1,8 +1,27 @@
+import { REGISTER_FAIL, REGISTER_LOADING, REGISTER_SUCCESS } from "../../constants/actionsTypes";
+
 const registrationReducer = (state, {type, payload}) =>{
 
   switch (type){
-    case "SIGUP":
-      return state;
+    case REGISTER_LOADING:
+      return {
+        ...state, 
+        loading: true
+      };
+
+    case REGISTER_SUCCESS:
+      return {
+        ...state, 
+        loading: false, 
+        data:payload
+      };
+
+    case REGISTER_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error:payload 
+      };
     
     default :
     return state;
