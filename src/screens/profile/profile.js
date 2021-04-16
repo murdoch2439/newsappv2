60e3b051223387603b0bc94193d013f70e1715b3
mr2k Ic <murdochkhallz@gmail.com>
2021-04-16 18:49:32 +0100

    profile screen

--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -1,10 +1,50 @@
 import React from 'react'
-import {View, StyleSheet, Text} from 'react-native'
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native'
+import {  Feather } from '@expo/vector-icons';
+import ProfilePicture from '../../components/common/userProfile/profilePicture'
+import colors from '../../components/theme/colors'
+import UserInfoComponent from '../../components/common/userProfile/userInfo';
+import HeaderWithBackButton from '../../components/headerwithbackbutton';
 
-const UserProfileScreen = () =>{
+
+
+const UserProfileScreen = ({navigation}) =>{
+  const openDrawer = () =>{
+        navigation.goBack()
+    }
   return(
     <View style={styles.container}>
-      <Text>UserProfileScreen</Text>
+      {/* <HeaderBackButton  /> */}
+      <HeaderWithBackButton goBackToHome={openDrawer} />
+      
+      {/* <Text style={styles.title}>Profile</Text> */}
+        <ProfilePicture />
+      <View style={styles.contentSection}>
+          
+            
+            <UserInfoComponent 
+              title="Username" 
+              content="KellyLordsy" 
+              icon={
+                < Feather name="user" style={styles.icon} />
+              } 
+            />
+      </View>
+
+            <UserInfoComponent 
+              title="Telephone" 
+              content="+243 80000000" 
+              icon={
+                <Feather name="smartphone" style={styles.icon} />
+              } 
+            />
+          
+            
+        <TouchableOpacity 
+            >
+            <Text style={styles.supprimer}>Supprimer le compte</Text>
+          </TouchableOpacity>
+      
     </View>
   )
 }
@@ -12,7 +52,28 @@ const UserProfileScreen = () =>{
 const styles = StyleSheet.create({
   container:{
     flex:1,
+    backgroundColor:colors.primary,
   },
+  title:{
+    fontSize:25,
+    color:colors.white,
+  },
+  icon:{
+    color:colors.secondary,
+    fontSize:30,
+  },
+  userInfos:{
+    color:colors.white,
+    fontSize:18,
+    paddingHorizontal:50,
+
+  },
+  supprimer:{
+    color:colors.danger,
+    textAlign:'center',
+    fontSize:15,
+    paddingTop:50,
+  }
 })
 
 export default UserProfileScreen
