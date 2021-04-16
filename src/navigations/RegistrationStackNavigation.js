aecc47204545d4f24a54d3f638e5ecfde7fef497
mr2k Ic <murdochkhallz@gmail.com>
2021-04-16 19:04:32 +0100

    I changed the screen from confirmation screen to profile screen to see how it behaves

--- src/navigations/RegistrationStackNavigation.js
+++ src/navigations/RegistrationStackNavigation.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import FormScreen from '../screens/registration/formScreen';
 import ConfirmationScreen from '../screens/registration/confirmationScreen';
-import { CONFIRMATION, SIGN_UP } from '../constants/routeNames';
+import { CONFIRMATION, SIGN_UP, USER_PROFILE } from '../constants/routeNames';
+import UserProfileScreen from '../screens/profile/profile';
 
 const RegistrationStack = createStackNavigator();
 
@@ -11,7 +12,7 @@ const RegistrationStackNavigation = () => {
   return (
       <RegistrationStack.Navigator initialRouteName={SIGN_UP}>
         <RegistrationStack.Screen name={SIGN_UP} component={FormScreen} options={{headerShown:false}} />
-        <RegistrationStack.Screen name={CONFIRMATION} component={ConfirmationScreen}  options={{headerShown:false}}
+        <RegistrationStack.Screen name={USER_PROFILE} component={UserProfileScreen}  options={{headerShown:false}}
          />
          
       </RegistrationStack.Navigator>
