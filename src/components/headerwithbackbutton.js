8c21756b2e453547539d0cc1134f52709a5a4653
mr2k Ic <murdochkhallz@gmail.com>
2021-04-16 18:39:47 +0100

    adding a headerBack button

--- src/components/headerwithbackbutton.js
+++ src/components/headerwithbackbutton.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {View, StyleSheet, Image} from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
+import { HeaderBackButton } from '@react-navigation/stack';
+
 const HeaderWithBackButton = ({goBackToHome}) =>{
 
   
     
     return(
         <View style={styles.container}>
-            <MaterialCommunityIcons name="keyboard-backspace" style={styles.backButton} onPress={goBackToHome} />
+            {/* <MaterialCommunityIcons name="keyboard-backspace" style={styles.backButton} onPress={goBackToHome} /> */}
+            <HeaderBackButton style={styles.backButton} onPress={goBackToHome} />
         </View>
     )
 }
