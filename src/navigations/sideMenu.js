89b5558288c80e5119e10375a2aa0396929146c8
mr2k Ic <murdochkhallz@gmail.com>
2021-04-16 19:41:35 +0100

    Nothing to say about this one

--- src/navigations/sideMenu.js
+++ src/navigations/sideMenu.js
@@ -10,8 +10,8 @@ const SideMenu = ({navigation}) =>{
 
 
   const menuItems = [
-    {icon:<Feather name="user-plus" size={24} color="black" />, name:"Registration", onPress : () => {
-      return navigation.navigate('Registration')}},
+    {icon:<Feather name="user-plus" size={24} color="black" />, name:"Profile", onPress : () => {
+      return navigation.navigate('User')}},
     {icon:<MaterialIcons name="home" size={24} color="black" />, name:"Home", onPress : ()=> {navigation.navigate('Home')}},
     {icon:<Fontisto name="bookmark" size={24} color="black" />, name:"Bookmark", onPress : ()=> {navigation.navigate('A lire plus tard')}},
     {icon:<MaterialIcons name="notifications-none" size={24} color="black" />, name:"Notifications", onPress : ()=> {navigation.navigate('Notifications')}},
