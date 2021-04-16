e470fbed071e618ded8915cb49ec8c17f27a6a48
mr2k Ic <murdochkhallz@gmail.com>
2021-04-16 19:00:00 +0100

    Updating the url

--- src/api/key.js
+++ src/api/key.js
@@ -1,25 +1,25 @@
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-// import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
 
-// let headers = {};
-
-
-// const axiosInstance = axios.create({
-//   baseURL: 'https://7sur7.cd/api/v1/articles',
-//   headers,
-// });
+let headers = {};
 
 
-// axiosInstance.interceptors.request.use(
-//   async (config) =>{
-//     const token =  await AsyncStorage.getItem("token")
-//     if(token){
-//       config.headers.Authorization = `Bearer ${token}`
-//     }
-//     return config
-//   }, (error) =>{
-//     return Promise.reject(error)
-//   }
-// )
+const axiosInstance = axios.create({
+  baseURL: 'http://b143f33bb93c.ngrok.io',
+  headers,
+});
 
-// export default axiosInstance
+
+axiosInstance.interceptors.request.use(
+  async (config) =>{
+    const token =  await AsyncStorage.getItem("token")
+    if(token){
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  }, (error) =>{
+    return Promise.reject(error)
+  }
+)
+
+export default axiosInstance
