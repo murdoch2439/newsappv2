3ad692e89c9cf9015ce72d3f76863046efc32d84
mr2k Ic <murdochkhallz@gmail.com>
2021-04-20 20:55:32 +0100

    Adding async storage to keep the info about the id, username and phone number, then check if they are the same to keep the user logged if they are false it means that this user has not yet any account

--- src/context/actions/registration/register.js
+++ src/context/actions/registration/register.js
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axiosInstance from "../../../api/key"
 // import axios from 'axios';
 import ngrokServer from "../../../api/ngrokServer";
@@ -7,6 +8,27 @@ export default ({ userName: user_name, phoneNumber: phone_number }) => dispatch 
 
   dispatch({type: REGISTER_LOADING})
   axiosInstance.post('/register', {user_name, phone_number}).then(res =>{
+    console.log('res.data : >>', res.data);
+    
+    const storeId = async (_id) => {
+      try {
+          await AsyncStorage.setItem('_id', res.data._id)
+      } catch (error) {
+          console.log(error);
+        }
+    }
+    const storeData = async (user) => {
+      try {
+          await AsyncStorage.setItem('user', JSON.stringify(res.data.user_name, res.data.user_name) )
+      } catch (error) {
+          console.log(error);
+        }
+    }
+    storeId()
+    storeData()
+
+    // AsyncStorage.setItem('_id', res.data._id)
+    // AsyncStorage.setItem('user', JSON.stringify(res.data.user))
     dispatch({
       type: REGISTER_SUCCESS,
       payload:res.data,
