dea3280e16480595bba5e48dfb664769c616ae16
mr2k Ic <murdochkhallz@gmail.com>
2021-04-21 15:04:17 +0100

    Many changes some of them are a little bit tricky to explain at the moment but I will find a better way to make it sense

--- src/navigations/DrawerNavigation.js
+++ src/navigations/DrawerNavigation.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeNavigation from './HomeNavigation'
 import NotificationsScreen from '../screens/notificationScreen'
@@ -9,6 +9,8 @@ import UserProfileScreen from '../screens/profile/profile';
 import SideMenu from './SideMenu';
 import { ABOUT, HOME_NAVIGATION, NOTIFICATIONS, REGISTRATION, SAVED_INFOS, USER_PROFILE } from '../constants/routeNames';
 import {GlobalContext} from '../context/Provider/'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ActivityIndicator } from 'react-native';
 // import HeaderWithBackButton from './components/headerwithbackbutton'
 
 
@@ -22,18 +24,45 @@ import {GlobalContext} from '../context/Provider/'
         
 const Drawer = createDrawerNavigator();
 
-const DrawerNavigation = () =>{
-        
+const DrawerNavigation = ({registrationDispatch}) =>{
+
         const {registrationState:{isLoggedIn}} = useContext(GlobalContext)
 
+        const [isAuthenticated, setIsAuthenticated] = useState(isLoggedIn)
+        const [authLoaded, setAuthLoaded] = useState(false)
+        
 
-        console.log("isLoggedIn : >>", isLoggedIn);
+        const getUser = async () =>{
+                try{
+                        const user =  await AsyncStorage.getItem('user')
+                                if(user){
+                                setAuthLoaded(true)
+
+                                        setIsAuthenticated(true)
+                                }else{
+                                   setAuthLoaded(true)
+
+                                        setIsAuthenticated(false)
+                                }
+                        
+                }catch(error){
+                        console.log(error);
+
+                }
+        }
+        useEffect(()=>{
+                getUser()
+        }, [isLoggedIn])
+
+        console.log("isLoggedIn : >>", isAuthenticated);
 
 
         return(
-                
+        <>
+
+        {authLoaded ? 
         <Drawer.Navigator initialRouteName={HOME_NAVIGATION} >     
-                {isLoggedIn ? <Drawer.Screen  name={USER_PROFILE} component={UserProfileScreen} /> : <Drawer.Screen  name={REGISTRATION} component={RegistrationStackNavigation} /> }
+                { isAuthenticated ? <Drawer.Screen  name={USER_PROFILE} component={UserProfileScreen} registrationDispatch={registrationDispatch} /> : <Drawer.Screen  name={REGISTRATION} component={RegistrationStackNavigation} /> }
                 {/* <Drawer.Screen  name="Registration" component={RegistrationStack} />
                 <Drawer.Screen  name="User" component={UserProfileScreen} /> */}
                 <Drawer.Screen name={HOME_NAVIGATION} component={HomeNavigation} />
@@ -42,6 +71,9 @@ const DrawerNavigation = () =>{
                 <Drawer.Screen name={ABOUT} component={AboutStackNavigation} />
                 {/* <Drawer.Screen name="Notifications" component={NotificationsScreen} options={{ header: ({navigation})=> <HeaderWithBackButton navigation={navigation} />}}/> */}
         </Drawer.Navigator>
+        : <ActivityIndicator />
+        }
+        </>
         )
 
 }
