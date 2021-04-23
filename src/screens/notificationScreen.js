2a5dafa220cb65c689cfcda986e476a1b712394a
mr2k Ic <murdochkhallz@gmail.com>
2021-04-23 19:04:54 +0100

    design change

--- src/screens/notificationScreen.js
+++ src/screens/notificationScreen.js
@@ -4,33 +4,32 @@ import {View,StyleSheet,Text, Image} from 'react-native';
 import ButtonNoBgd from '../components/common/boutons/ButtonNobgd';
 import HeaderWithBackButton from '../components/headerwithbackbutton'
 import colors from '../components/theme/colors';
-import { REGISTRATION, SIGN_UP } from '../constants/routeNames';
+import { REGISTRATION } from '../constants/routeNames';
 import { GlobalContext } from '../context/Provider';
 import { navigate } from '../navigations/RootNavigator';
 
 const NotificationsScreen = ({navigation}) =>{
-  // const {navigation} = useNavigation()
   const {registrationState:{isLoggedIn}} = useContext(GlobalContext)
 
   const goBackToHome = () =>{
         navigation.goBack()
-    }
+  }
 
   const onPressHandler = () =>{
-    
-      navigate(SIGN_UP)
-      // navigation.navigate(REGISTRATION)
-    
+      navigate(REGISTRATION)
   }
+
+
   return(
     <View style={styles.container}>
       <HeaderWithBackButton goBackToHome={goBackToHome} title="Notifications" />
-      
-      {/* <Text>Notification Screen</Text> */}
-      {/* {console.log('This is :', isLoggedIn)} */}
+
       {
         isLoggedIn ?
-        <Text>There's nothing yet for you!</Text>:
+        <View style={styles.notification}>
+
+          <Text style={styles.notificationText}>We have nothing for you yet!</Text>
+        </View>:
         <View style={styles.mainSection}>
           <Image style={styles.image} source={require('../../assets/notification.png')} />
           <Text style={styles.text}>You need to be logged in for receiving notifications</Text>
@@ -44,24 +43,31 @@ const NotificationsScreen = ({navigation}) =>{
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor:colors.grey
+    backgroundColor:colors.grey,
   },
   mainSection:{
-    // padding:10,
+    marginTop:'10%',    
   },
   image:{
     height:320,
     width:'100%',
     alignSelf:'center',
     marginTop:10,
-    
   },
   text:{
     fontSize:15,
     paddingTop:5,
     textAlign:'center'
-
   },
+  notification:{
+    flex:1,
+    justifyContent:'center',
+  },
+  notificationText:{
+    fontSize:20,
+    color:colors.accent,
+    textAlign:'center',
+  }
   
 })
 
